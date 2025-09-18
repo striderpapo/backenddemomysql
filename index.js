0176f7733e795e7b78b5a98267e00ca1cf1fa386
striderpapo <78055747+striderpapo@users.noreply.github.com>
2025-09-18 14:41:27 -0600

    clever activado dos

--- index.js
+++ index.js
@@ -4,19 +4,19 @@ var app = require('./app');
 var port = 3700;
 
 // Agregue las credenciales para acceder a su base de datos
-/*var connection = mysql.createConnection({
+var connection = mysql.createConnection({
     host     : "biosgvc6986ah0fdgsyv-mysql.services.clever-cloud.com",
     user     : "u6ct9w1jqqti0onf",
     password : "Eb6BoFN0xzFQLrsGUaT9",
     database : "biosgvc6986ah0fdgsyv"
-});*/
+});
 
-var connection = mysql.createConnection({
+/*var connection = mysql.createConnection({
     host     : 'localhost',
     user     : 'root',
     password : null,
     database : 'demonodemysql'
-});
+});*/
 
 // conectarse a mysql
 connection.connect(function(err) {
